2eacd1367742553842566b9c8e15335b02387dd3
Sebastiano Di Bella <sebydibella@gmail.com>
2026-06-21 15:42:58 +0200

    Fix CI: clean up testthat.R and add stats imports to NAMESPACE

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,3 @@
 library(testthat)
 
-# Source the package functions directly (pre-package structure)
-source(file.path(dirname(dirname(getwd())), "card_functions.R"), local = TRUE)
-source(file.path(dirname(dirname(getwd())), "signatures_genes_card.R"), local = TRUE)
-
 test_check("GeneCardAnalysis")
